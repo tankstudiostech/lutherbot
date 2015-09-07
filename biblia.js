0491289611978a176ad4f03307464555cec43b06
Ryan Tankersley <ryan.tankersley@interworks.com>
2015-09-07 15:14:11 -0500

    got getverse working

--- biblia.js
+++ biblia.js
@@ -1,18 +1,36 @@
 var request = require('request');
 
 var token = 'af3e9dd726db10140bb80cedd99863f8';
+var defaultToken = 'fd37d8f28e95d3be8cb4fbc37e15e18e';
 var bibliaUrl = "https://api.biblia.com/v1/bible";
 
 this.getVerse = function(text, callback) {
 	console.log("Getting Reference from " + text);
-	this.getReferenceFromText(text, function(reference) {
-		callback(reference);
+	this.getReferenceFromText(text, function(data) {
+		var res = JSON.parse(data);
+		console.log("Received reference: " + res.results);
+		if(res.results.length > 0)
+		{
+			getTextFromReference(res.results[0].passage, function(newData) {
+				text = JSON.parse(newData);
+				var sendBack = {
+					"text": text.text,
+					"reference": res.results[0].passage
+				}
+				console.log("Sending back: " + sendBack.reference + " " + sendBack.text)
+				callback(sendBack);
+			});
+		}
+		else
+		{
+			callback("");
+		}
 	});
 };
 
 this.getReferenceFromText = function(text, callback) {
 	console.log("Inside getReferenceFromText: " + text);
-	var url = bibliaUrl + '/scan.js?text=' + text + '&key=' + token;
+	var url = bibliaUrl + '/scan.text?text=' + text + '&key=' + token;
 	
 	var options = {
 		proxy: process.env.https_proxy,
@@ -20,8 +38,22 @@ this.getReferenceFromText = function(text, callback) {
 		method: 'GET'	
 	};
 	
-	console.log("Posting to " + url);
+	console.log("GET to " + url);
 	request.get(options, function (err, res, body) {
 		callback(body);
 	});
-}
+};
+
+function getTextFromReference(reference, callback) {
+	var url = bibliaUrl + '/content/LEB.txt.json?passage=' + reference + '&key=' + token;
+	var options = {
+		proxy: process.env.https_proxy,
+		url: url,
+		method: 'GET'	
+	};
+	
+	console.log("GET to " + url);
+	request.get(options, function (err, res, body) {
+		callback(body);
+	});
+};
--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var request = require('request');
 var biblia = require('./biblia');
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 app.listen(process.env.PORT || 5000);
 app.post('/getverse', function (req, res) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   var text = req.body.text;
   if (text != undefined) {
     console.log("Body Text: " + text);
@@ -22,7 +23,3 @@ app.post('/getverse', function (req, res) {
     res.send("Text not defined in body");
   }
 });
-
-this.sendResponse = function(res) {
-  
-}
